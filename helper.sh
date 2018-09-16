bc3ebdf11596822c4db626f515571b54ca908952
abhibesu2018 <abhibesu@gmail.com>
2018-09-16 11:44:15 +1000

    Added logging inside map running in the executor

--- helper.sh
+++ helper.sh
@@ -12,4 +12,6 @@ mkdir -p /tmp/spark-events
 cd /home/abhi/Downloads/spark-2.3.0-bin-hadoop2.7
 ./sbin/start-history-server.sh
 cd /home/abhi/eclipse-welcome/sparkscalaapplication/target/scala-2.11
+
+#Submitting spark job in deploy mode
 spark_submit --class com.abhi.sparkscala.ExampleJob --driver-memory 1g --executor-memory 1g --executor-cores 1 --num-executors 1 --driver-java-options "-Dlog4j.configuration=file:/home/abhi/eclipse-welcome/sparkscalaapplication/driver_log4j.properties" --master yarn --deploy-mode cluster sparkscalaapplication_2.11-0.1.0-SNAPSHOT.jar hdfs://localhost:9000/input/transactions.txt hdfs://localhost:9000/input/users.txt hdfs://localhost:9000/output
--- src/main/scala/com/abhi/sparkscala/ExampleJob.scala
+++ src/main/scala/com/abhi/sparkscala/ExampleJob.scala
@@ -1,31 +1,33 @@
 package com.abhi.sparkscala
 
-import org.apache.spark.SparkContext
-import org.apache.spark.SparkContext._
-import org.apache.spark.SparkConf
-import org.apache.spark.rdd.RDD
 import scala.collection.Map
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
-import java.io.Serializable
-import org.apache.log4j.LogManager
+
 import org.apache.log4j.Level
+import org.apache.log4j.LogManager
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import org.apache.spark.rdd.RDD
+import org.apache.spark.rdd.RDD.rddToPairRDDFunctions
+
 
 class ExampleJob(sc: SparkContext) {
   def run(t: String, u: String): RDD[(String, String)] = {
     val transactions = sc.textFile(t)
     val newTransactionsPair = transactions.map { t =>
       val p = t.split("\t")
-      /*logger.info("Print transaction pairs.....")
-      logger.info("print :"+p(2).toInt)
-      logger.info("print :"+p(1).toInt)*/
+      val logger = LogManager.getRootLogger
+      logger.setLevel(Level.INFO)
+      logger.info("Inside map:")
+      println("Print transaction pairs.....")
+      println("print :"+p(2).toInt)
+      println("print :"+p(1).toInt)
       (p(2).toInt, p(1).toInt)
     }
 
     val users = sc.textFile(u)
     val newUsersPair = users.map { t =>
       val p = t.split("\t")
-//      logger.info("Print users pairs.....")
+      println("New users pairs.....")
       println(p(0).toInt)
       println(p(3))
       (p(0).toInt, p(3))
@@ -37,9 +39,13 @@ class ExampleJob(sc: SparkContext) {
   def processData(t: RDD[(Int, Int)], u: RDD[(Int, String)]): Map[Int, Long] = {
     val logger = LogManager.getRootLogger
     logger.setLevel(Level.INFO)
+    println("Left outer join values:")
+    var jn1 = t.leftOuterJoin(u).values
+    jn1.collect().foreach(println)
     var jn = t.leftOuterJoin(u).values.distinct
     logger.info("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
     logger.info(jn)
+    jn.collect().foreach(println)
     logger.info("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
     return jn.countByKey
   }
